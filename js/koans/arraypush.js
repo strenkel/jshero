8b0c56fcd901cc03c571c08fdf4eca86112f76b2
strenkel <info@stefantrenkel.de>
2016-12-12 00:07:46 +0100

    arraypush fertiggestellt, util array.js geadded

--- js/koans/arraypush.js
+++ js/koans/arraypush.js
@@ -1,13 +1,15 @@
+(function(array) {
+
 jshero.koans.add({
 
   id: 'arraypush',
 
-  title: 'Array.shift und Array.push',
+  title: 'Array.shift() und Array.push()',
 
-  lesson: 'Arrays besitzen eine Reihe von Methoden. Zwei dieser Methoden möchte ich jetzt vorstellen: <code>shift</code> und <code>push</code>. ' +
+  lesson: 'Arrays besitzen eine Reihe von Methoden. Zwei dieser Methoden sind <code>shift</code> und <code>push</code>. ' +
     'Zur Erinnerung: Methoden sind Funktionen, die auf einem Objekt, hier Array, ausgeführt werden. Methoden werden mit einem Punkt ' +
     'an das Objekt angehängt. Wie bei Funktionen üblich folgt dem Methodennamen die runden Klammern, ggf. gefüllt mit Parametern. ' +
-    '<code>Array.shift</code> hat keine Parameter und verschiebt die Elemente eines Arrays nach links. ' +
+    '<code>Array.shift()</code> hat keine Parameter und verschiebt die Elemente eines Arrays nach links. ' +
     'Das 1te Element wird zum 0ten, das 2te zum 1ten, das 3te zum 2ten, usw. ' +
     'Das 0te Element wird aus dem Array entfernt und zurückgegeben. Nach dem <code>shift</code> hat das Array ein Element weniger.' +
     '<pre><code>var sprachen = ["C", "Java", "JavaScript"];<br>' +
@@ -15,29 +17,36 @@ jshero.koans.add({
     'Das Array <code>sprachen</code> wird mit 3 Programmiersprachen angelegt. Nach dem <code>shift</code> enthält <code>sprachen</code> ' +
     'nur noch 2 Elemente, nämlich <code>["Java", "JavaScript"]</code>. Der Wert <code>"C"</code> ist in der Variablen ' +
     '<code>naechsteSprache</code> gelandet.<br>' +
-    'Mit der häufig verwendeten Methode <code>Array.push</code> fügt man dem Array ein neues Element hinzu.' +
+    'Mit der häufig verwendeten Methode <code>Array.push()</code> fügt man dem Array ein neues Element hinzu.' +
+    'Das neue Element wird dabei an das Ende des Arrays angehängt.' +
     '<pre><code>var sprachen = [];<br>' +
     'sprachen.push("C");<br>' +
     'sprachen.push("Java");<br>' +
-    'sprachen.push("JavaScript");</code></pre>',
+    'sprachen.push("JavaScript");</code></pre>' +
+    'Hier wird ein leeres Array nacheinander mit den aufgeführten Werten gefüllt. Am Ende erhalten wir das Array ' +
+    '<code>["C", "Java", "JavaScript"]</code>. Wir hatten gesehen, dass man ein Array auch per Index füllen kann. ' +
+    '<code>sprachen[sprachen.length] = "PHP"</code> ist dasselbe wie <code>sprachen.push("PHP")</code>. Doch schon ' +
+    'an der Länge des Codes sieht man, dass die Variante mit <code>push</code> einfacher ist.',
         
-  task: '',
+  task: 'Schreibe ein Funktion <code>rotiere</code>, die die Elemente eines Arrays rotiert. Alle Elemente des Arrays sollen um eine ' +
+    'Position nach links verschoben werden. Das 0te Element soll ans Ende des Arrays gestellt werden. Das rotierte Array soll zurückgegeben werden. ' +
+    '<code>rotiere(["a", "b", "c"])</code> sollte <code>["b", "c", "a"]</code> ergeben.',
 
   beforeTests: function() {
-    if (typeof sortiere !== "undefined") {
-      sortieren = undefined;
+    if (typeof rotiere !== "undefined") {
+      rotiere = undefined;
     }
   },
 
   tests: [
     
     function() {
-      var ok = typeof sortiere === 'function';
+      var ok = typeof rotiere === 'function';
       var msg;
       if (ok) {
-        msg = '<code>sortiere</code> ist eine Funktion.';
+        msg = '<code>rotiere</code> ist eine Funktion.';
       } else {
-        msg = '<code>sortiere</code> ist keine Funktion.';
+        msg = '<code>rotiere</code> ist keine Funktion.';
       }
       return {
         ok: ok,
@@ -46,12 +55,12 @@ jshero.koans.add({
     },
 
     function() {
-      var ok = sortiere.length === 1;
+      var ok = rotiere.length === 1;
       var msg;
       if (ok) {
-        msg = '<code>sortiere</code> hat 1 Parameter.';
+        msg = '<code>rotiere</code> hat 1 Parameter.';
       } else {
-        msg = '<code>sortiere</code> hat nicht 1, sondern ' + sortiere.length + ' Parameter.';
+        msg = '<code>rotiere</code> hat nicht 1, sondern ' + rotiere.length + ' Parameter.';
       }
       return {
         ok: ok,
@@ -62,43 +71,49 @@ jshero.koans.add({
     function() {
       var ok, msg;
       try {
-        var result = sortiere([3, 2, 1]);
-        ok = Array.isArray(result) && result.length === 3 && result[0] === 1 && result[1] === 2 && result[2] === 3;
+        var result = rotiere([1, 2]);
+        ok = array.isEqual(result, [2, 1]);
         if (ok) {
-          msg = '<code>sortiere([3, 2, 1])</code> gibt <code>[1, 2, 3]</code> zurück.';
+          msg = '<code>rotiere([1, 2])</code> gibt <code>[2, 1]</code> zurück.';
         } else {
-          msg = '<code>sortiere([3, 2, 1])</code> gibt nicht <code>[1, 2, 3]</code>, sondern <code>' + JSON.stringify(result) + '</code> zurück.';
+          msg = '<code>rotiere([1, 2])</code> gibt nicht <code>[2, 1]</code>, sondern <code>' + JSON.stringify(result) + '</code> zurück.';
         }
       } catch(e) {
         ok = false;
-        msg = 'Fehler beim Aufruf von <code>sortiere([3, 2, 1])</code>.';
+        msg = 'Fehler beim Aufruf von <code>rotiere([1, 2])</code>.';
+        console.log(msg, e);
       }
       return {
         ok: ok,
         msg: msg
       };
     },
-   
+
     function() {
       var ok, msg;
       try {
-        var result = sortiere([2, 3, 1]);
-        ok = Array.isArray(result) && result.length === 3 && result[0] === 1 && result[1] === 2 && result[2] === 3;
+        var result = rotiere(["a", "b", "c"]);
+        ok = array.isEqual(result, ["b", "c", "a"]);
         if (ok) {
-          msg = '<code>sortiere([2, 3, 1])</code> gibt <code>[1, 2, 3]</code> zurück.';
+          msg = '<code>rotiere(["a", "b", "c"])</code> gibt <code>["b", "c", "a"]</code> zurück.';
         } else {
-          msg = '<code>sortiere([2, 3, 1])</code> gibt nicht <code>[1, 2, 3]</code>, sondern <code>' + JSON.stringify(result) + '</code> zurück.';
+          msg = '<code>rotiere(["a", "b", "c"])</code> gibt nicht <code>["b", "c", "a"]</code>, sondern <code>' + JSON.stringify(result) + '</code> zurück.';
         }
       } catch(e) {
         ok = false;
-        msg = 'Fehler beim Aufruf von <code>sortiere([2, 3, 1])</code>.';
+        msg = 'Fehler beim Aufruf von <code>rotiere(["a", "b", "c"])</code>.';
+        console.log(msg, e);
       }
       return {
         ok: ok,
         msg: msg
       };
     }
+   
+    
 
   ]
 
 });
+
+})(jshero.array);
