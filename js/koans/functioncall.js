2d9c54118bbbb9bf449b7ebbd27418b65f7ae245
Gregor Mi <codestruct@posteo.org>
2017-11-12 14:27:10 +0100

    koans/functioncall.js: Beschreibung deutlicher machen; factor out test code
    Ich habe versucht, den Beschreibungstext der Lektion und der Aufgabe so
    anzupassen, dass er besser zu verstehen ist. In diesem Lernstadium hat
    der Schüler aber sowieso nur eine sehr vage Vorstellung, was eine
    Funktion überhaupt ist und wird nur die Hälfte verstehen.
    
    Weiterhin habe ich den Testcode ausgelagert, so dass der Koan-Code viel
    kürzer wird.

--- js/koans/functioncall.js
+++ js/koans/functioncall.js
@@ -5,87 +5,36 @@ jshero.koans.add({
   title: "Funktionsaufrufe",
 
   lesson: `Wichtig ist der Unterschied zwischen der Definition und dem Aufruf einer Funktion.
-    Mit der Definition wird festgelegt, was die Funktion machen soll. Sie wird dabei nicht ausgeführt. Sie wird lediglich zur Ausführung bereitgestellt.
-    Erst mit ihrem Aufruf wird die Funktion ausgeführt. Dann werden die Anweisung im Funktionsblock der Reihe nach abgearbeitet.
-    Definiert werden Funktionen mit dem Schlüsselwort <code>function</code>. Ist die Funktion einer Variablen zugewiesen, geschiet der Aufruf der Funktion über diese Variable.
+    Mit der Definition (z. B. <code>var f = function() { return "hallo"; }</code>) wird festgelegt, was die Funktion machen soll. Sie wird dabei nicht ausgeführt. Sie wird lediglich zur Ausführung bereitgestellt.
+    Erst mit ihrem Aufruf (z. B. <code>f()</code>) wird die Funktion ausgeführt. Dann werden die Anweisung im Funktionsblock der Reihe nach abgearbeitet.
+    Definiert werden Funktionen mit dem Schlüsselwort <code>function</code>. Ist die Funktion einer Variablen zugewiesen, geschieht der Aufruf der Funktion über diese Variable.
     Funktionen werden einmal definiert, um sie dann an verschiedenen Programmstellen durch ihren Aufruf zu nutzen.`,
 
-  task: `Deklariere eine Variable <code>begruesse</code> und weise ihr eine Funktion zu, die <code>"Moin!"</code> zurückgibt.
-    Deklariere eine Variable <code>begruessung</code>. Rufe die Funktion auf und weise der Variablen <code>begruessung</code> die Rückgabe dieses Aufrufs zu.`,
+  task: `1. Definiere eine Funktion <code>begruesse</code>, die den Wet <code>"Moin!"</code> zurückgibt.
+  <br/>
+    2. Deklariere eine Variable <code>begruessung</code>. Rufe die Funktion <code>begruesse</code> auf und weise der Variablen <code>begruessung</code> die Rückgabe dieses Aufrufs zu. (Erinnerung: eine Zuweisung erfolgt mit dem <code>=</code>-Zeichen, genauso wie bei Variablen)`,
 
   beforeTests: function() {
-    if (typeof begruesse !== "undefined") {
-      hallo = undefined;
-    }
-    if (typeof begruessung !== "undefined") {
+      begruesse = undefined;
       begruessung = undefined;
-    }
   },
 
   tests: [
 
     function() {
-      var ok = typeof begruesse === 'function';
-      var msg;
-      if (ok) {
-        msg = '<code>begruesse</code> ist eine Funktion.';
-      } else {
-        msg = '<code>begruesse</code> ist keine Funktion.';
-      }
-      return {
-        ok: ok,
-        msg: msg
-      };
+        return jshero.testutil.assert_de_isFunction(begruesse);
     },
 
     function() {
-      var ok, msg, e;
-      try {
-        var result = begruesse();
-        ok = result === "Moin!";
-        if (ok) {
-          msg = '<code>begruesse()</code> gibt <code>"Moin!"</code> zurück.';
-        } else {
-          msg = '<code>begruesse()</code> gibt nicht <code>"Moin!"</code>, sondern <code>' + JSON.stringify(result) + '</code> zurück.';
-        }
-      } catch(exc) {
-        ok = false;
-        msg = "Fehler beim Aufruf von <code>hallo()</code>.";
-        e = exc;
-      }
-      return {
-        ok: ok,
-        msg: msg,
-        e: e
-      };
+        return jshero.testutil.assert_de_functionReturns(begruesse, "Moin!");
     },
 
     function() {
-      var ok = typeof begruessung !== 'undefined';
-      var msg;
-      if (ok) {
-        msg = "<code>begruessung</code> hat einen Wert.";
-      } else {
-        msg = "<code>begruessung</code> hat keinen Wert.";
-      }
-      return {
-        ok: ok,
-        msg: msg
-      };
+        return jshero.testutil.assert_de_variableDefined(begruessung, "begruessung");
     },
 
     function() {
-      var ok = begruessung === "Moin!";
-      var msg;
-      if (ok) {
-        msg = '<code>begruessung</code> hat den Wert "Moin!".';
-      } else {
-        msg = '<code>begruessung</code> hat nicht den Wert <code>"Moin!"</code>, sondern den Wert <code>' + JSON.stringify(begruessung) + '</code>.';
-      }
-      return {
-        ok: ok,
-        msg: msg
-      };
+        return jshero.testutil.assert_de_variableHasValue(begruessung, "begruessung", "Moin!");
     }
   ]
 
--- js/lib/testutil.js
+++ js/lib/testutil.js
@@ -53,9 +53,43 @@ jshero.testutil = (function() {
       };
     };
 
+    /**
+     * Bei der Variable v müssen wir noch mitgeben wie sie heißt (name)
+     */
+    var assert_de_variableDefined = function(v, name) {
+      var ok = typeof v !== 'undefined';
+      console.log(v);
+      var msg;
+      if (ok) {
+        msg = '<code>' + name + '</code> hat einen Wert.';
+      } else {
+        msg = '<code>' + name + '</code> hat keinen Wert.';
+      }
+      return {
+        ok: ok,
+        msg: msg
+      };
+    };
+
+    var assert_de_variableHasValue = function(v, name, expValue) {
+      var ok = begruessung === expValue;
+      var msg;
+      if (ok) {
+        msg = '<code>' + name + '</code> hat den Wert <code>' + JSON.stringify(v) + '</code>.';
+      } else {
+        msg = '<code>' + name + '</code> hat nicht den Wert <code>"' + expValue + '"</code>, sondern den Wert <code>' + JSON.stringify(v) + '</code>.';
+      }
+      return {
+        ok: ok,
+        msg: msg
+      };
+    }
+
   return {
     assert_de_isFunction: assert_de_isFunction,
     assert_de_functionReturns: assert_de_functionReturns,
+    assert_de_variableDefined: assert_de_variableDefined,
+    assert_de_variableHasValue: assert_de_variableHasValue
   };
 
 })();
