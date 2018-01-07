85888f204741022eb35d6ba71e99fe19a68addac
Stefan Trenkel <info@stefantrenkel.de>
2018-01-07 17:35:38 +0100

    change to multiline

--- js/koans/round.js
+++ js/koans/round.js
@@ -4,21 +4,21 @@ jshero.koans.add({
 
   title: "Runden",
 
-  lesson: 'Möchte man eine Dezimalzahl auf- oder abrunden, stehen einem die Funktionen <code>Math.round()</code>, ' +
-    '<code>Math.floor()</code> (engl.: Boden) und <code>Math.ceil()</code> (engl: Decke) zur Verfügung.' +
-    '<pre><code>' +
-    'var a = Math.round(5.49);<br>' +
-    'var b = Math.round(4.5);<br>' +
-    'var c = Math.floor(5.99);<br>' +
-    'var d = Math.ceil(4.01);' +
-    '</code></pre>' +
-    '<code>Math.round()</code> rundet kaufmännisch. Zahlen mit der ersten Dezimalstelle kleiner gleich 4 werden abgerundet, alle anderen aufgerundet. ' +
-    '<code>Math.floor()</code> rundet immer ab und <code>Math.ceil()</code> rundet immer auf. Die Variablen <code>a</code> bis <code>d</code> ' +
-    'erhalten also alle den Wert <code>5</code>.',
+  lesson: `Möchte man eine Dezimalzahl auf- oder abrunden, stehen einem die Funktionen <code>Math.round()</code>,
+<code>Math.floor()</code> (engl.: Boden) und <code>Math.ceil()</code> (engl: Decke) zur Verfügung.
 
-  task: 'Schreibe eine Funktion <code>round100</code>, die eine Dezimalzahl kaufmännisch auf die Hunderterstelle rundet. ' +
-    '<code>round100(1749)</code> sollte <code>1700</code> und <code>round100(856.123)</code> sollte <code>900</code> ergeben.<br>' +
-    'Tipp: Verwende neben <code>Math.round()</code> noch Multiplikation und Division.',
+<pre><code>var a = Math.round(5.49);
+var b = Math.round(4.5);
+var c = Math.floor(5.99);
+var d = Math.ceil(4.01);</code></pre>
+
+<code>Math.round()</code> rundet kaufmännisch. Zahlen mit der ersten Dezimalstelle kleiner gleich 4 werden abgerundet, alle anderen aufgerundet.
+<code>Math.floor()</code> rundet immer ab und <code>Math.ceil()</code> rundet immer auf. Die Variablen <code>a</code> bis <code>d</code>
+erhalten also alle den Wert <code>5</code>.`,
+
+  task: `Schreibe eine Funktion <code>round100</code>, die eine Dezimalzahl kaufmännisch auf die Hunderterstelle rundet.
+<code>round100(1749)</code> sollte <code>1700</code> und <code>round100(856.123)</code> sollte <code>900</code> ergeben.
+Tipp:&nbsp;Verwende neben <code>Math.round()</code> noch Multiplikation und Division.`,
 
   beforeTests: function() {
     if (typeof round100 !== "undefined") {
