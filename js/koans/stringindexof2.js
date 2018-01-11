ad095abbdece21824834b429b34ebe2b69bf7c76
Stefan Trenkel <info@stefantrenkel.de>
2018-01-11 21:55:43 +0100

    Kleine Text/Format Korrekturen.

--- js/koans/stringindexof2.js
+++ js/koans/stringindexof2.js
@@ -4,20 +4,23 @@ jshero.koans.add({
 
   title: 'String: indexOf(), 2. Parameter',
 
-  lesson: `Der Methode <code>indexOf</code> kann man als zweiten Parameter noch die Stelle mitgeben, ab der der Suchstring im aufrufenden String gesucht werden soll.
-    <pre><code>var n1 = "Fred Feuerstein".indexOf("e", 2);
+  lesson: `Der Methode <code>indexOf</code> kann man als zweiten Parameter noch die Stelle mitgeben,
+ab der der Suchstring im aufrufenden String gesucht werden soll.
+
+<pre><code>var n1 = "Fred Feuerstein".indexOf("e", 2);
 var n2 = "Fred Feuerstein".indexOf("e", 3);
 var n3 = "Fred Feuerstein".indexOf("e", 7);</code></pre>
-    Das erste <code>"e"</code> befindet sich in <code>"Fred Feuerstein"</code> an 2. Stelle. Es wird bei einer Suche ab der 2. Stelle gefunden. <code>n1</code>
-    ist also 2. Das zweite <code>"e"</code> in <code>"Fred Feuerstein"</code> befindet sich an der 6. Stelle. Es wird bei der Suche ab der 3. Stelle gefunden.
-    <code>n2</code> ist <code>6</code>. Mit der dritten Codezeile wird das dritte <code>"e"</code> in <code>"Fred Feuerstein"</code> gefunden.
-    <code>n3</code> ist <code>8</code>.`,
+
+Das erste <code>"e"</code> befindet sich in <code>"Fred Feuerstein"</code> an 2. Stelle. Es wird bei einer Suche ab der 2. Stelle gefunden. <code>n1</code>
+ist also 2. Das zweite <code>"e"</code> in <code>"Fred Feuerstein"</code> befindet sich an der 6. Stelle. Es wird bei der Suche ab der 3. Stelle gefunden.
+<code>n2</code> ist <code>6</code>. Mit der dritten Codezeile wird das dritte <code>"e"</code> in <code>"Fred Feuerstein"</code> gefunden.
+<code>n3</code> ist <code>8</code>.`,
 
   task: `Schreibe eine Funktion <code>secondIndexOf</code>, die 2 Strings entgegennimmt und die zweite Position des
-    zweiten Strings im ersten String bestimmt. Kommt der Suchstring nicht zweimal vor, so soll <code>-1</code> zurückgegeben werden.
-    <code>secondIndexOf("Maria Maria", "Maria")</code> soll <code>6</code> zurückgeben.
-    <br/>Tipp: Falls Du nicht weiterkommst, lese dir auch die Beschreibung in
-    <a href="https://wiki.selfhtml.org/wiki/JavaScript/Objekte/String/indexOf">SELFHTML</a> durch.`,
+zweiten Strings im ersten String bestimmt. Kommt der Suchstring nicht zweimal vor, so soll <code>-1</code> zurückgegeben werden.
+<code>secondIndexOf("Maria Maria", "Maria")</code> soll <code>6</code> zurückgeben.
+<br/>Tipp: Falls Du Hilfe brauchst, lese dir auch die Beschreibung in
+<a href="https://wiki.selfhtml.org/wiki/JavaScript/Objekte/String/indexOf">SELFHTML</a> durch.`,
 
   beforeTests: function() {
     if (typeof secondIndexOf !== "undefined") {
