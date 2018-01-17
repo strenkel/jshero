1ae875a1ea00c3f0a5cc224670adceb0df1d2e6f
Stefan Trenkel <info@stefantrenkel.de>
2018-01-17 22:18:11 +0100

    Multistring for substr

--- js/koans/stringsubstr.js
+++ js/koans/stringsubstr.js
@@ -4,17 +4,19 @@ jshero.koans.add({
 
   title: 'String: substr()',
 
-  lesson: 'Die Methode <code>substr</code> extrahiert aus einen String einen Teilstring:' +
-    '<pre><code>' +
-    'var see = "see and stop".substr(0, 3);<br>' +
-    'var and = "see and stop".substr(4, 3);<br>' +
-    'var stop = "see and stop".substr(8);<br>' +
-    '</code></pre>' +
-    'Der erste Parameter gibt an, von welcher Stelle an der Substring extrahiert werden soll. Der zweite Parameter gibt an, wieviel Zeichen ab der festgelegten Stelle extrahiert werden sollen. ' +
-    'Ist der zweite Paramter nicht angegeben, so wird immer bis zum Ende des ursprüglichen Strings extrahiert.',
+  lesson: `Die Methode <code>substr</code> extrahiert aus einen String einen Teilstring:
 
-  task: 'Schreibe eine Funktion <code>firstWord</code>, die einen String entgegennimmt und das erste Wort in diesem String zurückgibt. Das erste Wort sind dabei alle Zeichen bis zum ' +
-    'ersten Leerzeichen. <code>firstWord("Wo ist hier")</code> sollte <code>"Wo"</code> zurückgeben.',
+<pre><code>var see = "see and stop".substr(0, 3);
+var and = "see and stop".substr(4, 3);
+var stop = "see and stop".substr(8);</code></pre>
+
+Der erste Parameter gibt an, von welcher Stelle an der Substring extrahiert werden soll.
+Der zweite Parameter gibt an, wieviel Zeichen ab der festgelegten Stelle extrahiert werden sollen.
+Ist der zweite Paramter nicht angegeben, so wird immer bis zum Ende des ursprüglichen Strings extrahiert.`,
+
+  task: `Schreibe eine Funktion <code>firstWord</code>, die einen String entgegennimmt und das erste Wort
+in diesem String zurückgibt. Das erste Wort sind dabei alle Zeichen bis zum ersten Leerzeichen.
+<code>firstWord("Wo ist hier")</code> sollte <code>"Wo"</code> zurückgeben.`,
 
   beforeTests: function() {
     if (typeof firstWord !== "undefined") {
@@ -61,7 +63,7 @@ jshero.koans.add({
         } else {
           msg = '<code>firstWord("Wo ist hier")</code> gibt nicht <code>"Wo"</code>, sondern <code>' + JSON.stringify(result) + '</code> zurück.';
         }
-      } catch(exc) {
+      } catch (exc) {
         ok = false;
         msg = 'Fehler beim Aufruf von <code>firstWord("Wo ist hier")</code>.';
         e = exc;
@@ -83,7 +85,7 @@ jshero.koans.add({
         } else {
           msg = '<code>firstWord("Irres Licht")</code> gibt nicht <code>"Irres"</code>, sondern <code>' + JSON.stringify(result) + '</code> zurück.';
         }
-      } catch(exc) {
+      } catch (exc) {
         ok = false;
         msg = 'Fehler beim Aufruf von <code>firstWord("Irres Licht")</code>.';
         e = exc;
