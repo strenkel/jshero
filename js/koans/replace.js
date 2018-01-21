89037a96fc0572efb8370ae9cf12e251ddc3ca71
Stefan Trenkel <info@stefantrenkel.de>
2018-01-21 22:44:41 +0100

    Use multiline.

--- js/koans/replace.js
+++ js/koans/replace.js
@@ -4,26 +4,27 @@ jshero.koans.add({
 
   title: 'String: replace()',
 
-  lesson: 'Die Methode <code>replace</code> ersetzt einen Teilstring durch einen anderen:' +
-    '<pre><code>' +
-    'var str = "Hallo Marcel!";<br>' +
-    'var newstr = str.replace("Marcel", "Robert");' +
-    '</code></pre>' +
-    '"Marcel" wird durch "Robert" ersetzt. <code>newstr</code> hat also den Wert <code>"Hallo Robert!"</code>. ' +
-    'Der ursprüngliche String bleibt unverändert. Nur das erste Vorkommen wird ersetzt:' +
-    '<pre><code>' +
-    'var newname = "Peter".replace("e", "a");' +
-    '</code></pre>' +
-    '<code>newname</code> enthält den Wert <code>"Pater"</code> und nicht <code>"Patar"</code>. Ein Spezialfall ergibt sich, wenn der ersetzende String ' +
-    'der Leerstring ist:' +
-    '<pre><code>' +
-    'var nurso = "sowie so".replace("wie", "");' +
-    '</code></pre>' +
-    'Der zu ersetzende String wird entfernt. <code>nurso</code> enthält den Wert <code>"so so"</code>. Kommt der zu ersetzende String nicht vor, so wird der ' +
-    'ursprüngliche String zurückgegeben.',
+  lesson: `Die Methode <code>replace</code> ersetzt einen Teilstring durch einen anderen:
 
-  task: 'Schreibe eine Funktion <code>convert</code>, die einen Datumsstring in englischer Schreibweise (20-05-2017 oder 20/05/2017) in ' +
-    'deutsche Schreibweise (20.05.2017) umwandet. Sowohl <code>convert("20-05-2017")</code> als auch <code>convert("20/05/2017")</code> sollten <code>"20.05.2017"</code> ergeben.',
+<pre><code>var str = "Hallo Marcel!";
+var newstr = str.replace("Marcel", "Robert");</code></pre>' +
+
+"Marcel" wird durch "Robert" ersetzt. <code>newstr</code> hat also den Wert <code>"Hallo Robert!"</code>.
+Der ursprüngliche String bleibt unverändert. Nur das erste Vorkommen wird ersetzt:
+
+<pre><code>var newname = "Peter".replace("e", "a");</code></pre>
+
+<code>newname</code> enthält den Wert <code>"Pater"</code> und nicht <code>"Patar"</code>.
+Ein Spezialfall ergibt sich, wenn der ersetzende String der Leerstring ist:
+
+<pre><code>var nurso = "sowie so".replace("wie", "");</code></pre>
+
+Der zu ersetzende String wird entfernt. <code>nurso</code> enthält den Wert <code>"so so"</code>.
+Kommt der zu ersetzende String nicht vor, so wird der ursprüngliche String zurückgegeben.`,
+
+  task: `Schreibe eine Funktion <code>convert</code>, die einen Datumsstring in englischer Schreibweise (20-05-2017 oder 20/05/2017) in
+deutsche Schreibweise (20.05.2017) umwandet. Sowohl <code>convert("20-05-2017")</code> als auch <code>convert("20/05/2017")</code>
+sollten <code>"20.05.2017"</code> ergeben.`,
 
   beforeTests: function() {
     if (typeof convert !== "undefined") {
@@ -33,19 +34,19 @@ jshero.koans.add({
 
   tests: [
     function() {
-      return jshero.testutil.assert_isFunction('convert ');
+      return jshero.testutil.assert_isFunction('convert');
     },
 
     function() {
-      return jshero.testutil.assert_functionHasNumOfParameter('convert ', 1);
+      return jshero.testutil.assert_functionHasNumOfParameter('convert', 1);
     },
 
     function() {
-      return jshero.testutil.assert_functionReturns('convert ("10-07-1871")', '10.07.1871');
+      return jshero.testutil.assert_functionReturns('convert("10-07-1871")', '10.07.1871');
     },
 
     function() {
-      return jshero.testutil.assert_functionReturns('convert ("8/11/1922")', '8.11.1922');
+      return jshero.testutil.assert_functionReturns('convert("8/11/1922")', '8.11.1922');
     }
 
   ]
