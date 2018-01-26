7994621a453ba3fdb36a972a0300661ba043e136
Stefan Trenkel <info@stefantrenkel.de>
2018-01-26 23:46:17 +0100

    Use multiline.

--- js/koans/even.js
+++ js/koans/even.js
@@ -6,9 +6,9 @@ jshero.koans.add({
 
   lesson: 'Diese Aufgabe soll das Zusammenspiel von strikter Gleichheit und dem Modulo-Operator vertiefen.',
 
-  task: 'Schreibe eine Funktion <code>istGerade</code>, die angibt, ob eine übergebene Zahl gerade oder ungerade ist. ' +
-    'Ist die übergebene Zahl gerade, so soll die Funktion <code>true</code> zurückgeben, andernfalls <code>false</code>. ' +
-    '<code>istGerade(6)</code> soll <code>true</code> ergeben und <code>istGerade(7)</code> soll <code>false</code> zurückgeben. ',
+  task: `Schreibe eine Funktion <code>istGerade</code>, die angibt, ob eine übergebene Zahl gerade oder ungerade ist.
+Ist die übergebene Zahl gerade, so soll die Funktion <code>true</code> zurückgeben, andernfalls <code>false</code>.
+<code>istGerade(6)</code> soll <code>true</code> ergeben und <code>istGerade(7)</code> soll <code>false</code> zurückgeben.`,
 
   beforeTests: function() {
     if (typeof istGerade !== "undefined") {
