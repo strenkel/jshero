c63a4398dea7980dc3ae3510b78390a3765f23d7
Stefan Trenkel <info@stefantrenkel.de>
2018-01-27 13:52:20 +0100

    change to multiline

--- js/koans/while.js
+++ js/koans/while.js
@@ -1,22 +1,26 @@
-(function() {
-
 jshero.koans.add({
 
   id: 'while',
 
   title: 'while-Schleife',
 
-  lesson: 'Die <code>while</code>-Schleife besitzt nur eine Schleifen-Bedingung. Der Schleifen-Code wird ausgeführt, solange die Bedingung <code>true</code> ergibt. ' +
-    'Ergibt die Bedingung <code>false</code>, wird die Schleife beendet und die Codeausführung wird nach der Schleife fortgesetzt.' +
-    '<pre><code>var space = "";<br>' +
-    'while (space.length < 3) {<br>' +
-    '  space = space + "x";<br>' +
-    '}</code></pre>' +
-    'Solange <code>space</code> weniger als 3 Zeichen besitzt, wird es um <code>"x"</code> ergänzt. ' +
-    'Hat <code>space</code> die Länge 3, wird die Schleife beendet. <code>space</code> hat dann den Wert <code>"xxx"</code>.',
+  lesson: `Die <code>while</code>-Schleife besitzt nur eine Schleifen-Bedingung.
+Der Schleifen-Code wird ausgeführt, solange die Bedingung <code>true</code> ergibt.
+Ergibt die Bedingung <code>false</code>, wird die Schleife beendet und die Codeausführung
+wird nach der Schleife fortgesetzt.
 
-  task: 'Schreibe eine Funktion <code>abstand</code>, die eine natürliche Zahl n als Parameter entgegennimmt und die einen String bestehend aus ' +
-    'n Leerzeichen zurückgibt. <code>abstand(1)</code> sollte <code>" "</code> ergeben.',
+<pre><code>var space = "";
+while (space.length < 3) {
+  space = space + "x";
+}</code></pre>
+
+Solange <code>space</code> weniger als 3 Zeichen besitzt, wird es um <code>"x"</code> ergänzt.
+Hat <code>space</code> die Länge 3, wird die Schleife beendet.
+<code>space</code> hat dann den Wert <code>"xxx"</code>.`,
+
+  task: `Schreibe eine Funktion <code>abstand</code>, die eine natürliche Zahl n als Parameter
+entgegennimmt und die einen String bestehend aus n Leerzeichen zurückgibt.
+<code>abstand(1)</code> sollte <code>" "</code> ergeben.`,
 
   beforeTests: function() {
     if (typeof abstand !== "undefined") {
@@ -47,5 +51,3 @@ jshero.koans.add({
   ]
 
 });
-
-})();
