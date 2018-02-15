4bdb7965600aebff6c04c37f209f8cc8c2cb4ce3
Stefan Trenkel <info@stefantrenkel.de>
2018-02-15 15:18:10 +0100

    use multiline; nicht spezifzierten testcase entfernt

--- js/koans/arguments.js
+++ js/koans/arguments.js
@@ -4,36 +4,38 @@ jshero.koans.add({
 
   title: 'Das arguments-Objekt',
 
-  lesson: 'Das <code>arguments</code>-Objekt ist eine in jeder Funktion verfügbare Variable. ' +
-    'Es ist ein Array ähnliches Objekt, das die übergebenen Argumente einer Funktion der Reihe nach enthält. ' +
-    '<pre><code>' +
-    'var add = function() {<br>' +
-    '  var arg0 = arguments[0];<br>' +
-    '  var arg1 = arguments[1];<br>' +
-    '  return arg0 + arg1;<br>' +
-    '};<br><br>' +
-    'var sum = add(1, 2);' +
-    '</code></pre>' +
-    'Hier wird <code>add</code> mit den Argumenten <code>1</code> und <code>2</code> aufgerufen. <code>arguments[0]</code> enthält also den Wert <code>1</code>, ' +
-    '<code>arguments[1]</code> den Wert <code>2</code>. ' +
-    'Mit dem <code>arguments</code>-Objekt kann man ohne Parameter auf die übergebenen Argumente zugreifen. ' +
-    'Das ist z.B. dann praktisch, wenn man eine Funktion schreiben möchte, die eine beliebige Anzahl von Argumenten verarbeiten soll.' +
-    '<pre><code>' +
-    'var add = function() {<br>' +
-    '  var sum = 0;<br>' +
-    '  for (var i = 0; i < arguments.length; i++) {<br>' +
-    '    sum = sum + arguments[i];<br>' +
-    '  }<br>' +
-    '  return sum;<br>' +
-    '};<br><br>' +
-    'var s1 = add(2, 4);<br>' +
-    'var s2 = add(2, 3, 5, 7);<br>' +
-    '</code></pre>' +
-    'Hier iterieren wir über alle übergebenen Argumente und summieren sie auf. <code>arguments.length</code> liefert dabei die Anzahl der übergebenen Argumente. ' +
-    'Nun können wir <code>add</code> mit einer beliebigen Anzahl von Argumenten aufrufen.',
+  lesson: `Das <code>arguments</code>-Objekt ist eine in jeder Funktion verfügbare Variable.
+Es ist ein Array ähnliches Objekt, das die übergebenen Argumente einer Funktion der Reihe nach enthält.
 
-  task: 'Schreibe eine Funktion <code>maximum</code>, die beliebig viele Zahlen entgegennimmt und das Maximum dieser Zahlen liefert. ' +
-    '<code>maximum(12, 68, 3, 52)</code> sollte <code>68</code> ergeben.',
+<pre><code>var add = function() {
+  var arg0 = arguments[0];
+  var arg1 = arguments[1];
+  return arg0 + arg1;
+};
+
+var sum = add(1, 2);</code></pre>
+
+Hier wird <code>add</code> mit den Argumenten <code>1</code> und <code>2</code> aufgerufen. <code>arguments[0]</code> enthält also den Wert <code>1</code>,
+<code>arguments[1]</code> den Wert <code>2</code>.
+Mit dem <code>arguments</code>-Objekt kann man ohne Parameter auf die übergebenen Argumente zugreifen.
+Das ist z.B. dann praktisch, wenn man eine Funktion schreiben möchte, die eine beliebige Anzahl von Argumenten verarbeiten soll.
+
+<pre><code>var add = function() {
+  var sum = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    sum = sum + arguments[i];
+  }
+  return sum;
+};
+
+var s1 = add(2, 4);
+var s2 = add(2, 3, 5, 7);</code></pre>
+
+Hier iterieren wir über alle übergebenen Argumente und summieren sie auf. <code>arguments.length</code> liefert dabei die Anzahl der übergebenen Argumente.
+Nun können wir <code>add</code> mit einer beliebigen Anzahl von Argumenten aufrufen.`,
+
+  task: `Schreibe eine Funktion <code>maximum</code>, die beliebig viele Zahlen entgegennimmt und das Maximum dieser Zahlen liefert.
+<code>maximum(12, 68, 3, 52)</code> sollte <code>68</code> ergeben.`,
 
   beforeTests: function() {
     if (typeof maximum !== "undefined") {
@@ -47,10 +49,6 @@ jshero.koans.add({
     },
 
     function() {
-      return jshero.testutil.assert_functionReturns('maximum()', '');
-    },
-
-    function() {
       return jshero.testutil.assert_functionReturns('maximum(1)', 1);
     },
 
@@ -59,11 +57,11 @@ jshero.koans.add({
     },
 
     function() {
-      return jshero.testutil.assert_functionReturns('maximum(1, 15)', 15);
+      return jshero.testutil.assert_functionReturns('maximum(1, 15, 3)', 15);
     },
 
     function() {
-      return jshero.testutil.assert_functionReturns('maximum(1, 7, 3, 9, 2)', 9);
+      return jshero.testutil.assert_functionReturns('maximum(-1, 0, 9, 8)', 9);
     }
   ]
 
