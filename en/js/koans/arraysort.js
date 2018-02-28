900a39bd53f77590fb55c4f88f749302f8bc5ba2
Stefan Trenkel <info@stefantrenkel.de>
2018-02-28 21:39:13 +0100

    Tests verbessert

--- en/js/koans/arraysort.js
+++ en/js/koans/arraysort.js
@@ -5,9 +5,9 @@ jshero.koans.add({
   title: 'Sorting arrays',
 
   lesson: 'Now we can sort arrays with given length.',
-        
-  task: 'Write a function <code>sort</code> that takes an array filled with 3 numbers and returns these 3 numbers sorted in ascending order as an array. ' +
-    '<code>sort([2,&nbsp;3,&nbsp;1])</code> should return <code>[1,&nbsp;2,&nbsp;3]</code>.',
+
+  task: `Write a function <code>sort</code> that takes an array filled with 3 numbers and returns these 3 numbers sorted in ascending order as an array.
+<code>sort([2,&nbsp;3,&nbsp;1])</code> should return <code>[1,&nbsp;2,&nbsp;3]</code>.`,
 
   beforeTests: function() {
     if (typeof sort !== "undefined") {
@@ -16,77 +16,36 @@ jshero.koans.add({
   },
 
   tests: [
-    
     function() {
-      var ok = typeof sort === 'function';
-      var msg;
-      if (ok) {
-        msg = '<code>sort</code> is a function.';
-      } else {
-        msg = '<code>sort</code> is not a function.';
-      }
-      return {
-        ok: ok,
-        msg: msg
-      };
+      return jshero.testutil.assert_isFunction('sort');
     },
 
     function() {
-      var ok = sort.length === 1;
-      var msg;
-      if (ok) {
-        msg = '<code>sort</code> has 1 parameter.';
-      } else {
-        msg = '<code>sort</code> has not 1 parameter, but ' + sort.length + '.';
-      }
-      return {
-        ok: ok,
-        msg: msg
-      };
+      return jshero.testutil.assert_functionHasNumOfParameter('sort', 1);
     },
 
     function() {
-      var ok, msg, e;
-      try {
-        var result = sort([3, 2, 1]);
-        ok = Array.isArray(result) && result.length === 3 && result[0] === 1 && result[1] === 2 && result[2] === 3;
-        if (ok) {
-          msg = '<code>sort([3, 2, 1])</code> returns <code>[1, 2, 3]</code>.';
-        } else {
-          msg = '<code>sort([3, 2, 1])</code> does not return <code>[1, 2, 3]</code>, but <code>' + JSON.stringify(result) + '</code>.';
-        }
-      } catch(exc) {
-        ok = false;
-        msg = 'Error when calling <code>sort([3, 2, 1])</code>.';
-        e = exc;
-      }
-      return {
-        ok: ok,
-        msg: msg,
-        e: e
-      };
+      return jshero.testutil.assert_functionReturns('sort([1, 2, 3])', [1, 2, 3]);
     },
-   
+
     function() {
-      var ok, msg, e;
-      try {
-        var result = sort([2, 3, 1]);
-        ok = Array.isArray(result) && result.length === 3 && result[0] === 1 && result[1] === 2 && result[2] === 3;
-        if (ok) {
-          msg = '<code>sort([2, 3, 1])</code> returns <code>[1, 2, 3]</code>.';
-        } else {
-          msg = '<code>sort([2, 3, 1])</code> does not return <code>[1, 2, 3]</code>, but <code>' + JSON.stringify(result) + '</code>.';
-        }
-      } catch(exc) {
-        ok = false;
-        msg = 'Error when calling <code>sort([2, 3, 1])</code>.';
-        e = exc;
-      }
-      return {
-        ok: ok,
-        msg: msg,
-        e: e
-      };
+      return jshero.testutil.assert_functionReturns('sort([1, 3, 2])', [1, 2, 3]);
+    },
+
+    function() {
+      return jshero.testutil.assert_functionReturns('sort([2, 1, 3])', [1, 2, 3]);
+    },
+
+    function() {
+      return jshero.testutil.assert_functionReturns('sort([2, 3, 1])', [1, 2, 3]);
+    },
+
+    function() {
+      return jshero.testutil.assert_functionReturns('sort([3, 1, 2])', [1, 2, 3]);
+    },
+
+    function() {
+      return jshero.testutil.assert_functionReturns('sort([3, 2, 1])', [1, 2, 3]);
     }
 
   ]
--- js/koans/arraysort.js
+++ js/koans/arraysort.js
@@ -6,8 +6,8 @@ jshero.koans.add({
 
   lesson: 'Wir können jetzt Arrays mit vorgegebener Länge sortieren.',
 
-  task: 'Schreibe eine Funktion <code>sortiere</code>, die ein mit 3 Zahlen gefülltes Array entgegennimmt und die diese 3 Zahlen aufsteigend sortiert als Array  zurückgibt. ' +
-    '<code>sortiere([2,&nbsp;3,&nbsp;1])</code> sollte <code>[1,&nbsp;2,&nbsp;3]</code> ergeben.',
+  task: `Schreibe eine Funktion <code>sortiere</code>, die ein mit 3 Zahlen gefülltes Array entgegennimmt und die diese 3 Zahlen aufsteigend sortiert als Array  zurückgibt.
+<code>sortiere([2,&nbsp;3,&nbsp;1])</code> sollte <code>[1,&nbsp;2,&nbsp;3]</code> ergeben.`,
 
   beforeTests: function() {
     if (typeof sortiere !== "undefined") {
@@ -25,16 +25,29 @@ jshero.koans.add({
     },
 
     function() {
+      return jshero.testutil.assert_functionReturns('sortiere([1, 2, 3])', [1, 2, 3]);
+    },
+
+    function() {
+      return jshero.testutil.assert_functionReturns('sortiere([1, 3, 2])', [1, 2, 3]);
+    },
+
+    function() {
+      return jshero.testutil.assert_functionReturns('sortiere([2, 1, 3])', [1, 2, 3]);
+    },
+
+    function() {
+      return jshero.testutil.assert_functionReturns('sortiere([2, 3, 1])', [1, 2, 3]);
+    },
+
+    function() {
       return jshero.testutil.assert_functionReturns('sortiere([3, 1, 2])', [1, 2, 3]);
     },
 
     function() {
       return jshero.testutil.assert_functionReturns('sortiere([3, 2, 1])', [1, 2, 3]);
-    },
-
-    function() {
-      return jshero.testutil.assert_functionReturns('sortiere(["c", "a", "b"])', ["a", "b", "c"]);
     }
+
   ]
 
 });
