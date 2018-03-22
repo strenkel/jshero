d64c36fe33a2bf1dd33eca560c209f51955b1aaa
Stefan Trenkel <info@stefantrenkel.de>
2018-03-22 09:36:02 +0100

    Change to multiline and usage of testutil

--- en/js/koans/array2.js
+++ en/js/koans/array2.js
@@ -4,15 +4,15 @@ jshero.koans.add({
 
   title: 'Get array elements',
 
-  lesson: 'The elements of an array are accessed by an index beginning with 0. ' +
-    '<pre><code>var languages = ["C", "C++", "Java"];<br>' +
-    'var c = languages[0];<br>' +
-    'var cPlusPlus = languages[1];<br>' +
-    'var java = languages[2];' +
-    '</code></pre>',
-        
-  task: 'Write a function <code>getFirstElement</code> that takes an array and returns the first element of the array. ' +
-    '<code>getFirstElement([1, 2])</code> should return <code>1</code>.',
+  lesson: `The elements of an array are accessed by an index beginning with 0.
+
+<pre><code>var languages = ["C", "C++", "Java"];
+var c = languages[0];
+var cPlusPlus = languages[1];
+var java = languages[2];</code></pre>`,
+
+  task: `Write a function <code>getFirstElement</code> that takes an array and returns the first element of the array.
+<code>getFirstElement([1, 2])</code> should return <code>1</code>.`,
 
   beforeTests: function() {
     if (typeof getFirstElement !== "undefined") {
@@ -21,77 +21,21 @@ jshero.koans.add({
   },
 
   tests: [
-    
+
     function() {
-      var ok = typeof getFirstElement === 'function';
-      var msg;
-      if (ok) {
-        msg = '<code>getFirstElement</code> is a function.';
-      } else {
-        msg = '<code>getFirstElement</code> is not a function.';
-      }
-      return {
-        ok: ok,
-        msg: msg
-      };
+      return jshero.testutil.assert_isFunction('getFirstElement');
     },
 
     function() {
-      var ok = getFirstElement.length === 1;
-      var msg;
-      if (ok) {
-        msg = '<code>getFirstElement</code> has 1 parameter.';
-      } else {
-        msg = '<code>getFirstElement</code> has not 1 parameter, but ' + getFirstElement.length + '.';
-      }
-      return {
-        ok: ok,
-        msg: msg
-      };
+      return jshero.testutil.assert_functionHasNumOfParameter('getFirstElement', 1);
     },
-   
+
     function() {
-      var ok, msg, e;
-      try {
-        var result = getFirstElement([1, 2]);
-        ok = result === 1;
-        if (ok) {
-          msg = '<code>getFirstElement([1, 2])</code> returns <code>1</code>.';
-        } else {
-          msg = '<code>getFirstElement([1, 2])</code> does not return <code>1</code>, but <code>' + JSON.stringify(result) + '</code>.';
-        }
-      } catch(exc) {
-        ok = false;
-        msg = 'Error when calling <code>getFirstElement([1, 2])</code>.';
-        e = exc;
-      }
-      return {
-        ok: ok,
-        msg: msg,
-        e: e
-      };
+      return jshero.testutil.assert_functionReturns('getFirstElement([1, 2])', 1);
     },
- 
+
     function() {
-      var ok, msg, e;
-      try {
-        var result = getFirstElement(["Firefox", "Chrome"]);
-        ok = result === "Firefox";
-        if (ok) {
-          msg = '<code>getFirstElement(["Firefox", "Chrome"])</code> returns <code>"Firefox"</code>.';
-        } else {
-          msg = '<code>getFirstElement(["Firefox", "Chrome"])</code> does not return <code>"Firefox"</code>, but <code>' + JSON.stringify(result) + '</code>.';
-        }
-      } catch(exc) {
-        ok = false;
-        msg = 'Error when calling <code>getFirstElement(["Firefox", "Chrome"])</code>.';
-        e = exc;
-      }
-      return {
-        ok: ok,
-        msg: msg,
-        e: e
-      };
+      return jshero.testutil.assert_functionReturns('getFirstElement(["Firefox", "Chrome"])', "Firefox");
     }
 
   ]
