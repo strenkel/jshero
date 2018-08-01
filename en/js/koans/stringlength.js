fb6b96bb9267033ef782cc1381a7234231bbb5f1
Stefan Trenkel <info@stefantrenkel.de>
2018-08-01 08:21:28 +0200

    refactor: use testutils

--- en/js/koans/stringlength.js
+++ en/js/koans/stringlength.js
@@ -4,17 +4,22 @@ jshero.koans.add({
 
   title: "String: length",
 
-  lesson: 'Strings have the <code>length</code> property. This property represents the number of characters in a string. ' +
-    'To do this, you must append <code>.length</code> to the string or to the variable that contains the string:' +
-    '<pre><code>var charNumber = "Ai".length;</code/></pre>' +
-    '<code>"Ai"</code> has 2 characters. So <code>charNumber</code> has the value <code>2</code>.' +
-    '<pre><code>var name = "Weiwei";<br>charNumber = name.length;</code/></pre>' +
-    '<code>"Weiwei"</code> has 6 characters. <code>charNumber</code> now has the value <code>6</code>.' +
-    '<pre><code>charNumber = "".length;</code/></pre>' +
-    'The empty string has no characters. The value of <code>charNumber</code> is <code>0</code>.',
+  lesson: `Strings have the <code>length</code> property. This property represents the number of characters in a string.
+To do this, you must append <code>.length</code> to the string or to the variable that contains the string:
 
-  task: 'Write a function <code>length</code> that takes a string and returns the number of characters of the string. ' +
-    '<code>length("sun")</code> should return <code>3</code>.',
+<pre><code>var charNumber = "Ai".length;</code/></pre>
+
+<code>"Ai"</code> has 2 characters. So <code>charNumber</code> has the value <code>2</code>.
+
+<pre><code>var name = "Weiwei";
+charNumber = name.length;</code/></pre>
+
+<code>"Weiwei"</code> has 6 characters. <code>charNumber</code> now has the value <code>6</code>.
+<pre><code>charNumber = "".length;</code/></pre>
+The empty string has no characters. The value of <code>charNumber</code> is <code>0</code>.`,
+
+  task: `Write a function <code>length</code> that takes a string and returns the number of characters of the string.
+<code>length("sun")</code> should return <code>3</code>.`,
 
   beforeTests: function() {
     if (typeof length !== "undefined") {
@@ -23,78 +28,26 @@ jshero.koans.add({
   },
 
   tests: [
+
     function() {
-      var ok = typeof length === 'function';
-      var msg;
-      if (ok) {
-        msg = '<code>length</code> is a function.';
-      } else {
-        msg = '<code>length</code> is not a function.';
-      }
-      return {
-        ok: ok,
-        msg: msg
-      };
+      return jshero.testutil.assert_isFunction('length');
     },
 
     function() {
-      var result = length.length;
-      var ok = result === 1;
-      var msg;
-      if (ok) {
-        msg = '<code>length</code> has 1 parameter.';
-      } else {
-        msg = '<code>length</code> has not 1, but ' + result + ' parameters.';
-      }
-      return {
-        ok: ok,
-        msg: msg
-      };
+      return jshero.testutil.assert_functionHasNumOfParameter('length', 1);
     },
 
     function() {
-      var ok, msg, e;
-      try {
-        var result = length('Hello world!');
-        ok = result === 12;
-        if (ok) {
-          msg = '<code>length("Hello world!")</code> returns <code>12</code>.';
-        } else {
-          msg = '<code>length("Hello world!")</code> does not return <code>12</code>, but <code>' + JSON.stringify(result) + '</code>.';
-        }
-      } catch(exc) {
-        ok = false;
-        msg = 'Error when calling <code>length("Hello world!")</code>.';
-        e = exc;
-      }
-      return {
-        ok: ok,
-        msg: msg,
-        e: e
-      };
+      return jshero.testutil.assert_functionReturns('length("sun")', 3);
     },
-    
+
     function() {
-      var ok, msg, e;
-      try {
-        var result = length('');
-        ok = result === 0;
-        if (ok) {
-          msg = '<code>length("")</code> returns <code>0</code>.';
-        } else {
-          msg = '<code>length("")</code> does not return <code>0</code>, but <code>' + JSON.stringify(result) + '</code>.';
-        }
-      } catch(exc) {
-        ok = false;
-        msg = 'Error when calling <code>length("")</code>.';
-        e = exc;
-      }
-      return {
-        ok: ok,
-        msg: msg,
-        e: e
-      };
+      return jshero.testutil.assert_functionReturns('length("Hello world!")', 12);
+    },
+
+    function() {
+      return jshero.testutil.assert_functionReturns('length("")', 0);
     }
-  ]
 
+  ]
 });
