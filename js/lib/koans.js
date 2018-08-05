02b950144a19820e6e033714b5b45ebe20a60b10
Stefan Trenkel <info@stefantrenkel.de>
2018-08-05 10:53:17 +0200

    Refactor

--- js/lib/koans.js
+++ js/lib/koans.js
@@ -6,40 +6,38 @@ jshero.koans = (function() {
 
   var index = 0;
   var koans = [];
+  var koansMap = {};
 
   var add = function(koan) {
     koans.push(koan);
+    koansMap[koan.id] = koan;
   };
 
   /**
    * Pass an array of all koan ids that should appear in the tutorial.
    * This order in the array will define
    * the order in which the koans appear in the tutorial.
+   * Must be called AFTER all koans are added!
+   *
+   * @param {[koandId]} order
    */
-  var initReorder = function(idArray) {
+  var initReorder = function(order) {
 
-    if (idArray.length !== koans.length) {
-      // This should not happend in production.
+    if (order.length !== koans.length) {
       console.log("ERROR: Koans-Order-Array length unequal to Koans length!");
     }
 
     var koan;
-    var koansIdMap = {};
-    for (var i = 0, l = koans.length; i < l; i++) {
-      koan = koans[i];
-      koansIdMap[koan.id] = koan;
-    }
-
     koans = [];
-    for (var i = 0, l = idArray.length; i < l; i++) {
-      koan = koansIdMap[idArray[i]];
+    for (var i = 0, l = order.length; i < l; i++) {
+      koan = koansMap[order[i]];
       if (koan != null) {
         koans.push(koan);
       } else {
         console.log("ERROR: No koan for koan-Id in order array!");
       }
     }
-  }
+  };
 
   var getKoan = function() {
     return koans[index];
