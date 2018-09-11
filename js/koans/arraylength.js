4bb7071089b06a0dea9e80b9c31b2b01cd11920a
Stefan Trenkel <info@stefantrenkel.de>
2018-09-11 17:12:22 +0200

    fix too long test output

--- js/koans/arraylength.js
+++ js/koans/arraylength.js
@@ -1,10 +1,16 @@
-jshero.koans.add({
+(function() {
 
-  id: 'arraylength',
+  var random = function() {
+    return Math.round(Math.random() * 10);
+  };
 
-  title: 'Array: length',
+  jshero.koans.add({
 
-  lesson: `Die Eigenschaft <code>length</code> hatten wird schon bei Strings kennengelernt. Dort gab sie die Anzahl
+    id: 'arraylength',
+
+    title: 'Array: length',
+
+    lesson: `Die Eigenschaft <code>length</code> hatten wird schon bei Strings kennengelernt. Dort gab sie die Anzahl
 der Zeichen eines Strings zurück. Bei Arrays gibt sie die Anzahl der Elemente zurück.
 
 <pre><code>var sprachen = [];
@@ -18,53 +24,54 @@ Das leere Array zu Anfang enthält keine Elemente. <code>l0</code> ist also <cod
 Nun füllen wir das Array nach und nach. <code>l1</code> und <code>l2</code> haben dann die Werte <code>1</code> und <code>2</code>.
 Beachte: Die Länge eines Arrays ist immer um 1 größer als der höchste Index des Arrays.`,
 
-  task: `Schreibe eine Funktion <code>getLastElement</code>, die ein Array entgegennimmt und die das letzte Element des Arrays zurückgibt.
+    task: `Schreibe eine Funktion <code>getLastElement</code>, die ein Array entgegennimmt und die das letzte Element des Arrays zurückgibt.
 <code>getLastElement([1, 2])</code> sollte <code>2</code> zurückgeben.`,
 
-  beforeTests: function() {
-    if (typeof getLastElement !== "undefined") {
-      getLastElement = undefined;
-    }
-  },
-
-  tests: [
-    function() {
-      return jshero.testutil.assert_isFunction('getLastElement');
-    },
-
-    function() {
-      return jshero.testutil.assert_functionHasNumOfParameter('getLastElement', 1);
-    },
-
-    function() {
-      return jshero.testutil.assert_functionReturns('getLastElement([1, 2])', 2);
-    },
-
-    function() {
-      return jshero.testutil.assert_functionReturns('getLastElement(["Hänsel", "Gretel"])', 'Gretel');
-    },
-
-    function() {
-      return jshero.testutil.assert_functionReturns('getLastElement(["a", "b", "c"])', 'c');
-    },
-
-    function() {
-      return jshero.testutil.assert_functionReturns('getLastElement(["Maus", 1])', 1);
-    },
-
-    function() {
-      var testArray = new Array();
-      var testArrayString = '[';
-      var size = 5;
-      for (var i = 0; i < size - 1; i++) {
-        testArray[i] = Math.random();
-        testArrayString += testArray[i] + ', ';
+    beforeTests: function() {
+      if (typeof getLastElement !== "undefined") {
+        getLastElement = undefined;
       }
-      testArray[size - 1] = Math.random();
-      testArrayString += testArray[size - 1] + ']';
-      return jshero.testutil.assert_functionReturns('getLastElement(' + testArrayString + ')', testArray[size - 1]);
-    }
+    },
 
-  ]
+    tests: [
+      function() {
+        return jshero.testutil.assert_isFunction('getLastElement');
+      },
 
-});
+      function() {
+        return jshero.testutil.assert_functionHasNumOfParameter('getLastElement', 1);
+      },
+
+      function() {
+        return jshero.testutil.assert_functionReturns('getLastElement([1, 2])', 2);
+      },
+
+      function() {
+        return jshero.testutil.assert_functionReturns('getLastElement(["Hänsel", "Gretel"])', 'Gretel');
+      },
+
+      function() {
+        return jshero.testutil.assert_functionReturns('getLastElement(["a", "b", "c"])', 'c');
+      },
+
+      function() {
+        return jshero.testutil.assert_functionReturns('getLastElement(["Maus", 1])', 1);
+      },
+
+      function() {
+        var testArray = new Array();
+        var testArrayString = '[';
+        var size = 4;
+        for (var i = 0; i < size - 1; i++) {
+          testArray[i] = random();
+          testArrayString += testArray[i] + ', ';
+        }
+        testArray[size - 1] = random();
+        testArrayString += testArray[size - 1] + ']';
+        return jshero.testutil.assert_functionReturns('getLastElement(' + testArrayString + ')', testArray[size - 1]);
+      }
+    ]
+
+  });
+
+})();
