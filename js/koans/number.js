581d2c00fa234bcec7b5b80c3faab1e7d5570169
Stefan Trenkel <info@stefantrenkel.de>
2018-09-17 14:32:08 +0200

    add hint/solution

--- js/koans/number.js
+++ js/koans/number.js
@@ -4,16 +4,38 @@ jshero.koans.add({
 
   title: "Zahlen",
 
-  lesson: 'Zahlen werden in JavaScript durch einfache Ziffern dargestellt. Sie können einen Dezimalpunkt und ein Minuszeichen besitzen.' +
-    '<pre><code>var x1 = 1;<br>var x2 = 1.0;<br>var x3 = 3.14;<br>var x4 = -1;</code></pre>' +
-    '<code>1</code> und <code>1.0</code> sind dieselbe Zahl. ' +
-    'Mit Zahlen kann man rechnen. Die vier Grundrechenarten Addieren, Subtrahieren, Multiplizieren und Dividieren werden durch ' +
-    '<code>+ - *</code> und <code>/</code> dargestellt.' +
-    '<pre><code>var x1 = 6;<br>var x2 = 2;<br>var x3 = x1 + x2;<br>var x4 = x1 - x2;<br>var x5 = x1 * x2;<br>var x6 = x1 / x2;</code></pre>' +
-    'Die Variablen <code>x3</code> bis <code>x6</code> haben so die Werte <code>8</code>, <code>4</code>, <code>12</code> und <code>3</code>.',
+  lesson: `Zahlen werden in JavaScript durch einfache Ziffern dargestellt. Sie können einen
+Dezimalpunkt und ein Minuszeichen besitzen.
 
-  task: 'Schreibe eine Funktion <code>add</code>, die zwei Zahlen als Parameter entgegennimmt und deren Summe zurückgibt. ' +
-    '<code>add(1, 2)</code> sollte <code>3</code> ergeben.',
+<pre><code>var x1 = 1;
+var x2 = 1.0;
+var x3 = 3.14;
+var x4 = -1;</code></pre>
+
+<code>1</code> und <code>1.0</code> sind dieselbe Zahl. Mit Zahlen kann man rechnen. Die
+vier Grundrechenarten Addieren, Subtrahieren, Multiplizieren und Dividieren werden durch
+<code>+ - *</code> und <code>/</code> dargestellt.
+
+<pre><code>var x1 = 6;
+var x2 = 2;
+var x3 = x1 + x2;
+var x4 = x1 - x2;
+var x5 = x1 * x2;
+var x6 = x1 / x2;</code></pre>
+
+Die Variablen <code>x3</code> bis <code>x6</code> haben so die Werte <code>8</code>,
+<code>4</code>, <code>12</code> und <code>3</code>.`,
+
+  task: `Schreibe eine Funktion <code>add</code>, die zwei Zahlen als Parameter entgegennimmt
+und deren Summe zurückgibt. <code>add(1, 2)</code> sollte <code>3</code> ergeben.`,
+
+  hint: `<pre><code>var add = function(x, y) {
+  return ...
+};`,
+
+  solution: `<pre><code>var add = function(x, y) {
+  return x + y;
+};`,
 
   beforeTests: function() {
     if (typeof add !== "undefined") {
