d52266d1d5cc54419080b51d1a5869628af2b0d5
Stefan Trenkel <info@stefantrenkel.de>
2018-09-23 23:04:51 +0200

    Formatting

--- js/util/util.js
+++ js/util/util.js
@@ -6,7 +6,7 @@ jshero.util = (function() {
 
   // Does not work local with file:///...
   var isPhoneGap = function() {
-    return document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+    return document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1;
   };
 
   var scrollToBottom = function() {
@@ -25,10 +25,10 @@ jshero.util = (function() {
    *
    * return {string} die formatierte Nachricht.
    */
-  var formatMessage = function( message, values ) {
+  var formatMessage = function(message, values) {
     var result = message;
-    for (var i=0; i<values.length; i++) {
-      result = result.replace( "%"+i, values[i]);
+    for (var i = 0; i < values.length; i++) {
+      result = result.replace("%" + i, values[i]);
     }
     return result;
   };
