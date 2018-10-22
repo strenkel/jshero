8ffbb78907351026aa801a4296478bbeaf485f6d
Stefan Trenkel <info@stefantrenkel.de>
2018-10-22 22:03:52 +0200

    Refactor: add modul injection

--- en/js/koans/math.js
+++ en/js/koans/math.js
@@ -1,10 +1,12 @@
-jshero.koans.add({
+(function(testutil) {
 
-  id: "math",
+  jshero.koans.add({
 
-  title: "Math",
+    id: "math",
 
-  lesson: `Many mathematical functions are grouped together in the <code>Math</code> object.
+    title: "Math",
+
+    lesson: `Many mathematical functions are grouped together in the <code>Math</code> object.
 For example, <code>Math.sqrt(x)</code> returns the square root
 and <code>Math.pow(x, y)</code> calculates x to the power of y.
 
@@ -14,46 +16,48 @@ var y2 = Math.pow(10, 3);</code></pre>
 <code>y1</code> has the value <code>3</code> and <code>y2</code> has the value <code>1000</code>
 (10&sup3; = 10 * 10 * 10 = 1000).`,
 
-  task: `Write a function <code>hypotenuse</code> that calculates the length of the hypotenuse
+    task: `Write a function <code>hypotenuse</code> that calculates the length of the hypotenuse
 of a right triangle. The length of the two legs is passed to the function. Tip: In a right
 triangle the Pythagorean theorem is valid. If a and b are the lengths of the two legs and c
 is the length of the hypotenuse, the following is true: a&sup2; + b&sup2; = c&sup2;.
 Since 3&sup2; + 4&sup2; = 5&sup2; applies, <code>hypotenuse(3,4)</code> should return <code>5</code>.`,
 
-hint: `<pre><code>var hypotenuse = function(a, b) {
+    hint: `<pre><code>var hypotenuse = function(a, b) {
   var cQuadrat = a * a + b * b;
   return ...
 };</code></pre>`,
 
-  solution: `<pre><code>var hypotenuse = function(a, b) {
+    solution: `<pre><code>var hypotenuse = function(a, b) {
   var cQuadrat = a * a + b * b;
   return Math.sqrt(cQuadrat);
 };</code></pre>`,
 
-  beforeTests: function() {
-    if (typeof hypotenuse !== "undefined") {
-      hypotenuse = undefined;
-    }
-  },
-
-  tests: [
-
-    function() {
-      return jshero.testutil.assert_isFunction('hypotenuse');
+    beforeTests: function() {
+      if (typeof hypotenuse !== "undefined") {
+        hypotenuse = undefined;
+      }
     },
 
-    function() {
-      return jshero.testutil.assert_functionHasNumOfParameter('hypotenuse', 2);
-    },
+    tests: [
 
-    function() {
-      return jshero.testutil.assert_functionReturns('hypotenuse(3, 4)', 5);
-    },
+      function() {
+        return testutil.assert_isFunction('hypotenuse');
+      },
 
-    function() {
-      return jshero.testutil.assert_functionReturns('hypotenuse(5, 12)', 13);
-    }
+      function() {
+        return testutil.assert_functionHasNumOfParameter('hypotenuse', 2);
+      },
 
-  ]
+      function() {
+        return testutil.assert_functionReturns('hypotenuse(3, 4)', 5);
+      },
 
-});
+      function() {
+        return testutil.assert_functionReturns('hypotenuse(5, 12)', 13);
+      }
+
+    ]
+
+  });
+
+})(jshero.testutil);
