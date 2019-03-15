93aafca796c50695afb7c727408e4b326bffe756
Stefan Trenkel <info@stefantrenkel.de>
2019-03-15 17:02:05 +0100

    Build en/success.html

--- build/build.js
+++ build/build.js
@@ -3,13 +3,24 @@
 // All pathes relative to JS Hero root directory
 const fs = require('fs');
 const ejs = require('ejs');
-const koans = require("../www/js/koans.js");
+const koansDe = require("../www/js/koans.js");
+const koansEn = require("../www/en/js/koans.js");
 
-ejs.renderFile("src/html/success.html", { koans: koans.getKoans() }, function(err, successHtml) {
+// build de/success.html
+ejs.renderFile("src/html/de/success.html", { koans: koansDe.getKoans() }, function(err, successHtml) {
   if (err) throw err;
   fs.writeFile("www/success.html", successHtml, function(err) {
     if (err) throw err;
-    console.log("Build success.html");
+    console.log("Build success.html (de)");
+  });
+});
+
+// build en/success.html
+ejs.renderFile("src/html/en/success.html", { koans: koansEn.getKoans() }, function(err, successHtml) {
+  if (err) throw err;
+  fs.writeFile("www/en/success.html", successHtml, function(err) {
+    if (err) throw err;
+    console.log("Build success.html (en)");
   });
 });
 
