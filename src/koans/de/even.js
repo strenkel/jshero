b201eb1667653c8af548bc869dffef45d101a14e
Stefan Trenkel <strenkel@neusta.de>
2019-03-28 14:22:35 +0100

    Add hint/solution

--- src/koans/de/even.js
+++ src/koans/de/even.js
@@ -4,11 +4,17 @@ jshero.koans.add({
 
   title: 'Gerade Zahlen',
 
-  lesson: 'Diese Aufgabe soll das Zusammenspiel von strikter Gleichheit und dem Modulo-Operator vertiefen.',
-
   task: `Schreibe eine Funktion <code>istGerade</code>, die angibt, ob eine übergebene Zahl gerade oder ungerade ist.
 Ist die übergebene Zahl gerade, so soll die Funktion <code>true</code> zurückgeben, andernfalls <code>false</code>.
-<code>istGerade(6)</code> soll <code>true</code> ergeben und <code>istGerade(7)</code> soll <code>false</code> zurückgeben.`,
+Der Aufruf <code>istGerade(6)</code> soll <code>true</code> ergeben und <code>istGerade(7)</code> soll <code>false</code>
+zurückgeben.`,
+
+  hint: `Verwende den Modulo-Operator zusammen mit strikter Gleichheit, um zu bestimmen, ob eine Zahl
+gerade ist.`,
+
+  solution: `var istGerade = function(n) {
+  return n % 2 === 0;
+};`,
 
   beforeTests: function() {
     if (typeof istGerade !== "undefined") {
