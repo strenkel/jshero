55913e0e3a956900eff7c9151f04bb07456a0c26
Stefan Trenkel <info@stefantrenkel.de>
2019-05-22 07:19:43 +0200

    just formatting: line break after col 120

--- src/koans/de/arguments.js
+++ src/koans/de/arguments.js
@@ -15,10 +15,11 @@ Es ist ein Array ähnliches Objekt, das die übergebenen Argumente einer Funktio
 
 var sum = add(1, 2);</code></pre>
 
-Hier wird <code>add</code> mit den Argumenten <code>1</code> und <code>2</code> aufgerufen. <code>arguments[0]</code> enthält also den Wert <code>1</code>,
-<code>arguments[1]</code> den Wert <code>2</code>.
+Hier wird <code>add</code> mit den Argumenten <code>1</code> und <code>2</code> aufgerufen.
+<code>arguments[0]</code> enthält also den Wert <code>1</code>, <code>arguments[1]</code> den Wert <code>2</code>.
 Mit dem <code>arguments</code>-Objekt kann man ohne Parameter auf die übergebenen Argumente zugreifen.
-Das ist z.B. dann praktisch, wenn man eine Funktion schreiben möchte, die eine beliebige Anzahl von Argumenten verarbeiten soll.
+Das ist z.B. dann praktisch, wenn man eine Funktion schreiben möchte, die eine beliebige Anzahl von Argumenten
+verarbeiten soll.
 
 <pre><code>var add = function() {
   var sum = 0;
@@ -31,11 +32,12 @@ Das ist z.B. dann praktisch, wenn man eine Funktion schreiben möchte, die eine 
 var s1 = add(2, 4);
 var s2 = add(2, 3, 5, 7);</code></pre>
 
-Hier iterieren wir über alle übergebenen Argumente und summieren sie auf. <code>arguments.length</code> liefert dabei die Anzahl der übergebenen Argumente.
+Hier iterieren wir über alle übergebenen Argumente und summieren sie auf.
+<code>arguments.length</code> liefert dabei die Anzahl der übergebenen Argumente.
 Nun können wir <code>add</code> mit einer beliebigen Anzahl von Argumenten aufrufen.`,
 
-  task: `Schreibe eine Funktion <code>maximum</code>, die beliebig viele Zahlen entgegennimmt und das Maximum dieser Zahlen liefert.
-<code>maximum(12, 68, 3, 52)</code> sollte <code>68</code> ergeben.`,
+  task: `Schreibe eine Funktion <code>maximum</code>, die beliebig viele Zahlen entgegennimmt und das Maximum dieser
+Zahlen liefert. <code>maximum(12, 68, 3, 52)</code> sollte <code>68</code> ergeben.`,
 
   tests: [
     function() {
