dce7efd3da647d6eedc65d4108a1149f13778c3b
Stefan Trenkel <info@stefantrenkel.de>
2019-05-28 17:47:35 +0200

    Improve lesson example

--- src/koans/de/break.js
+++ src/koans/de/break.js
@@ -6,35 +6,37 @@ jshero.koans.add({
 
   lesson: `Mit <code>break</code> wird eine Schleife beendet. Das Programm wird nach der Schleife fortgesetzt.
 
-<pre><code>var woerter = 'blau weiß rot';
-for (var i = 0; i < woerter.length; i++) {
-  if (woerter.charAt(i) === ' ') {
+<pre><code>var words = 'blau weiß rot';
+var indexFirstSpace = -1;
+for (var i = 0; i < words.length; i++) {
+  if (words.charAt(i) === ' ') {
+    indexFirstSpace = i;
     break;
   }
-}
-var laengeErstesWort = i;</code></pre>
+}</code></pre>
 
-Ist das Zeichen mit dem Index <code>i</code> in <code>woerter</code> das Leerzeichen, so ist die
-<code>if</code>-Bedingung erfüllt und der <code>break</code>-Befehl wird ausgeführt. Die Schleife wird beendet und
-die Codeausführung wird nach der Schleife fortgesetzt. In unserem Beispiel ist das Zeichen mit dem Index <code>4</code>
-das Leerzeichen. Sobald die Schleifenvariable <code>i</code> den Wert <code>4</code> erreicht hat, wird die Schleife
-beendet. <code>laengeErstesWort</code> bekommt den Wert <code>4</code> zugewiesen.<br><br>
+Ist das Zeichen mit dem Index <code>i</code> in <code>words</code> das Leerzeichen, so ist die
+<code>if</code>-Bedingung erfüllt. <code>indexFirstSpace</code> bekommt den Laufindex <code>i</code> zugewisen
+und der <code>break</code>-Befehl wird ausgeführt. Die Schleife wird beendet.
+Die Codeausführung wird nach der Schleife fortgesetzt. In unserem Beispiel ist das Zeichen mit dem Index <code>4</code>
+das Leerzeichen. Sobald die Schleifenvariable <code>i</code> den Wert <code>4</code> erreicht hat,
+bekommt <code>indexFirstSpace</code> den Wert <code>4</code> zugewiesen und die Schleife wird beendet.<br><br>
 
 Mit <code>continue</code> wird der aktuelle Schleifendurchlauf beendet. Die Programmausführung springt zum Schleifenkopf.
 
-<pre><code>var summe = 0;
+<pre><code>var sum = 0;
 for (var i = 0; i < 10; i++) {
   if (i % 2 === 0) {
     continue;
   }
-  summe = summe + i;
+  sum = sum + i;
 }</code></pre>
 
 Ist <code>i</code> eine ungerade Zahl, so gilt: i modulo 2 = 1. Die <code>if</code> - Bedingung ist nicht erfüllt.
-Die Codeausführung wird nach dem <code>if</code> fortgesetzt und <code>i</code> wird zu <code>summe</code> hinzugezählt.
+Die Codeausführung wird nach dem <code>if</code> fortgesetzt und <code>i</code> wird zu <code>sum</code> hinzugezählt.
 Ist <code>i</code> eine gerade Zahl, so gilt: i modulo 2 = 0. Die <code>if</code> - Bedingung ist erfüllt und
 <code>continue</code> wird ausgeführt. Die Code-Ausführung springt direkt zum Schleifenkopf. <code>i</code> wird nicht
-zu <code>summe</code> hinzugezählt. So werden alle ungeraden Zahlen kleiner 10 aufaddiert.<br><br>
+zu <code>sum</code> hinzugezählt. So werden alle ungeraden Zahlen kleiner 10 aufaddiert.<br><br>
 <code>break</code> und <code>continue</code> kann man in allen Schleifen (for, while, do...while) verwenden.`,
 
   task: `Schreibe eine Funktion <code>isPrime</code>. Sie soll prüfen, ob eine übergebene Zahl eine Primzahl ist oder
