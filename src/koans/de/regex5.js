f433942f403ebb1d02cdf4d38d3c34e400144c8c
Stefan Trenkel <info@stefantrenkel.de>
2019-09-25 07:28:25 +0200

    fix error in regex5; small correction in regex7

--- src/koans/de/regex5.js
+++ src/koans/de/regex5.js
@@ -12,41 +12,44 @@ Der Abkürzung ist ein Backslash vorangestellt, damit sie nicht als normaler Buc
 gelesen wird. Abkürzungen kann man auch innerhalb einer Zeichenauswahl verwenden.
 <code>/[\\da-z]/</code> steht für eine Ziffer oder einen Kleinbuchstaben.`,
 
-  task: `Schreibe eine Funktion <code>hatPLZ</code>, die prüft, ob ein String eine
-deutsche Postleitzahl enthällt. Eine deutsche Postleitzahl ist eine Zahl mit 5 Ziffern.
-<code>hatPLZ('28205')</code> sollte <code>true</code> ergeben.`,
+  task: `Schreibe eine Funktion <code>test</code>, die prüft, ob ein String 5 aufeinanderfolgende
+Ziffern enthällt. <code>test('28205')</code> sollte <code>true</code> ergeben.`,
 
-  hint: `<pre><code>var hatPLZ = function(s) {
+  hint: `<pre><code>var test = function(s) {
 // Verwende \\d fünfmal.
 };</code></pre>`,
 
-  solution: `<pre><code>var hatPLZ = function(s) {
+  solution: `<pre><code>var test = function(s) {
   return /\\d\\d\\d\\d\\d/.test(s);
 };</code></pre>`,
 
   tests: [
     function() {
-      return jshero.testutil.assert_isFunction('hatPLZ');
+      return jshero.testutil.assert_isFunction('test');
     },
 
     function() {
-      return jshero.testutil.assert_functionHasNumOfParameter('hatPLZ', 1);
+      return jshero.testutil.assert_functionHasNumOfParameter('test', 1);
     },
 
     function() {
-      return jshero.testutil.assert_functionReturns("hatPLZ('28205 Bremen')", true);
+      return jshero.testutil.assert_functionReturns("test('28205 Bremen')", true);
     },
 
     function() {
-      return jshero.testutil.assert_functionReturns("hatPLZ('06493 Neudorf')", true);
+      return jshero.testutil.assert_functionReturns("test('06493 Neudorf')", true);
     },
 
     function() {
-      return jshero.testutil.assert_functionReturns("hatPLZ('2805 Bremen')", false);
+      return jshero.testutil.assert_functionReturns("test('123456')", true);
     },
 
     function() {
-      return jshero.testutil.assert_functionReturns("hatPLZ('006493 Neudorf')", false);
+      return jshero.testutil.assert_functionReturns("test('1000 kg')", false);
+    },
+
+    function() {
+      return jshero.testutil.assert_functionReturns("test('1000-1=999')", false);
     }
 
   ]
