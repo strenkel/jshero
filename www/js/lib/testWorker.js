bc3f62db5ff6b6e97eca16c8b5d21a9ef6196f79
Stefan Trenkel <info@stefantrenkel.de>
2019-10-21 10:31:25 +0200

    Version 3.0.5

--- www/js/lib/testWorker.js
+++ www/js/lib/testWorker.js
@@ -104,7 +104,7 @@ importScripts(
     var initKoans = function() {
       var koansUrl = language === "de" ? "../koans.js" : "../../en/js/koans.js";
       // add dummy version to prevent caching after new release
-      koansUrl += "?v=304"; 
+      koansUrl += "?v=305"; 
       importScripts(koansUrl);
     };
 
