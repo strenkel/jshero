b3741b62002ce4b3ab9077b49eba42dd9e0f09dd
Stefan Trenkel <info@stefantrenkel.de>
2019-12-19 09:09:15 +0100

    improve text

--- src/koans/dom/de/backgroundcolor.js
+++ src/koans/dom/de/backgroundcolor.js
@@ -7,10 +7,10 @@ jshero.koans.add({
   lesson: `Webseiten basieren auf HTML, der Hypertext Markup Language. HTML-Seiten bestehen aus Text und werden
 durch sogenannte Tags strukturiert. Tags stellen meist einen rechteckigen Bereich der Webseite dar.
 Diese Bereiche können mit CSS, den Cascading Style Sheets, formatiert werden. Mit CSS kann man die
-Position, die Größe, die Farbe und vieles mehr der einzelnen Tags festlegen. Eine gute Einführung in HTML
-und CSS findest Du auf <a href="https://wiki.selfhtml.org">SELFHTML</a>.<br><br>
+Position, die Größe, die Farbe und vieles mehr der einzelnen Tags festlegen. Eine Einführung in HTML
+und CSS findest du auf <a href="https://wiki.selfhtml.org">SELFHTML</a>.<br><br>
 
-Hier lernst Du, wie Du Webseiten mit Hilfe von JavaScript verändern kannst.
+Hier lernst du exemplarisch, wie man Webseiten mit Hilfe von JavaScript verändern kann.
 Solche Webseiten nennt man auch dynamische Webseiten. Ein gutes Beispiel für eine dynamische Webseite ist JS Hero.
 Sind auf einer Seite alle Tests grün, wechselt die Farbe der oberen Titelleiste von rot auf grün.
 Das geschiet mit Hilfe von JavaScript. Einen solchen Farbwechsel wollen wir jetzt programmieren.<br><br>
@@ -18,7 +18,8 @@ Das geschiet mit Hilfe von JavaScript. Einen solchen Farbwechsel wollen wir jetz
 Um einzelne Tags einer Webseite verändern zu können, benötigt JavaScript eine gesonderte Bibliothek,
 das Document Object Model (DOM). Diese Bibliothek steht in jedem Browser über das
 <code>document</code> Objekt zur Verfügung. Um auf ein Tag, auch HTML-Element genannt, zugreifen zu können,
-muss man es identifizieren. Das geht am besten, wenn man dem Tag im HTML Code eine ID zuweist.
+muss man es identifizieren. Das geht am besten, wenn man dem Tag im HTML Code eine ID, einen 
+<a href="https://de.wikipedia.org/wiki/Identifikator">Identifikator</a>, zuweist.
 Hat ein Tag die ID <code>content</code>, so sieht der Zugriff wie folgt aus:  
 
 <pre><code>var elm = document.getElementById("content");</code></pre>
