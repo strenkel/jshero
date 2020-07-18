072e14fde368cc898a9701d3dec7d13d2936a92e
Stefan Trenkel <strenkel@neusta.de>
2020-07-18 21:38:39 +0200

    wip: change to function declaration

--- src/koans/core/en/function.js
+++ src/koans/core/en/function.js
@@ -5,11 +5,14 @@ jshero.koans.add({
   title: 'Functions',
 
   lesson: `A function is a block of statements that can be executed as often as you like.
-A function can have parameters and can return a value.
-Functions are defined by the keyword <code>function</code>, followed by the parameters enclosed in parentheses,
-followed by the instructions enclosed in curly brackets.
-Use the keyword <code>return</code> to return a value. At the same time <code>return</code> terminates the function.
-Functions can be assigned to a variable. With
+A function can have parameters and can return a value. Functions can be defined in different ways.
+Perhaps the most common one is the function declaration. It starts with the keyword <code>function</code>.
+It follows:
+<ul><li>the name of the function,</li>
+<li>the parameters enclosed in round parentheses,</li>
+<li>the statements enclosed in curly brackets.</li></ul>
+With the keyword <code>return</code> a value can be returned. At the same time <code>return</code> terminates the function.
+Statements after a <code>return</code> will not be executed. With
 
 <pre><code>function color() {
   return 'red';
@@ -17,14 +20,14 @@ Functions can be assigned to a variable. With
 
 we define a function that returns the value <code>'red'</code>. The function has no parameters (the parentheses are empty)
 and contains only the return statement. The function is assigned to the variable <code>color</code>.
-The function is called via this variable:
+The function is called by its name:
 
 <pre><code>let result = color();</code></pre>
 
 Now the function is executed. Its return is assigned to the variable <code>result</code>. <code>result</code> therefore
 has the value <code>'red'</code>.`,
 
-  task: `Declare a variable <code>hello</code>. Assign a function to it returning <code>'Hello world!'</code>.`,
+  task: `Define a function <code>hello</code> that returns <code>'Hello world!'</code>.`,
 
   hint: `<pre><code>function hello() {
   ...
