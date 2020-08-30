c9e7919d8903af3c678ef2b35489e9e2f38e3f0d
Stefan Trenkel <info@stefantrenkel.de>
2020-08-30 10:57:12 +0200

    add silent teacher reference

--- src/koans/core/en/jsx07.js
+++ src/koans/core/en/jsx07.js
@@ -4,7 +4,14 @@
 
     id: 'jsx07',
 
-    title: 'What is x?',
+    title: 'Silent Teacher',
+
+    lesson: `A wonderful learn to code app is
+<a href="http://silentteacher.toxicode.fr/">SILENT TEACHER</a>. It's a minimalist game where beginners can discover
+what coding is all about and the key concepts behind code, with no explanations given. Through several series of
+questions, the player is led to guess the rules of this language and learn from his own mistakes.
+Since SILENT TEACHER uses JavaScript, it is an excellent supplement to JS Hero.<br><br>
+Here the concatenation of strings shall be practiced again.`,
 
     task: `Which value does <code>x</code> have after execution of the following code?
 <pre><code>function double(name) {
