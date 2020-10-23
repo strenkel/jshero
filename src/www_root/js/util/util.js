7ae9d52357ef814cd185354e788c4d97edd3841f
Stefan Trenkel <strenkel@neusta.de>
2020-10-23 14:18:14 +0200

    Refactor

--- src/www_root/js/util/util.js
+++ src/www_root/js/util/util.js
@@ -4,44 +4,44 @@ if (typeof jshero === "undefined") {
 
 jshero.util = (function() {
 
-  var scrollToBottom = function() {
+  function scrollToBottom() {
     window.scrollTo(0, document.body.scrollHeight);
   };
 
-  var scrollToTop = function() {
+  function scrollToTop() {
     window.scrollTo(0, 0);
   };
 
   /**
-   * Diese Funktion ersetzt die Platzhalter %0 bis %9 durch die Werte im übergebenen Wertearray.
+   * Replaces the placeholders %0, %1, %2, ... with the values in the array.
    *
-   * @param {string} message mit den Platzhaltern.
-   * @param {array} values für die Platzhalter.
+   * @param {string} message with placeholders.
+   * @param {array} values for the placeholders.
    *
-   * return {string} die formatierte Nachricht.
+   * return {string} the formatted message
    */
-  var formatMessage = function(message, values) {
-    var result = message;
-    for (var i = 0; i < values.length; i++) {
+  function formatMessage(message, values) {
+    let result = message;
+    for (let i = 0; i < values.length; i++) {
       result = result.replace("%" + i, values[i]);
     }
     return result;
   };
 
-  var onclick = function(elm, func) {
-    var myFunc = function() {
+  function onclick(elm, func) {
+    const myFunc = function() {
       func();
       return false;
-    }
+    };
     elm.onclick = myFunc;
     elm.ontouchstart = myFunc;
   };
 
-  return {
+  return Object.freeze({
     scrollToBottom: scrollToBottom,
     scrollToTop: scrollToTop,
     formatMessage: formatMessage,
     onclick: onclick
-  };
+  });
 
 })();
