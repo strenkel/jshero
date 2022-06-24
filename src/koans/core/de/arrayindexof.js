bbd624aaa82cd8f6daed417408c0d28c96967ee9
strenkel <info@stefantrenkel.de>
2022-06-24 15:37:38 +0200

    Improve koan de/arrayindexof

--- src/koans/core/de/arrayindexof.js
+++ src/koans/core/de/arrayindexof.js
@@ -15,10 +15,25 @@ let i2 = systems.indexOf('Windows');</code></pre>
 <code>systems</code> ist. <code>i2</code> erhält den Wert <code>-1</code>.
 Dieser Wert wird zurückgegeben, wenn sich das übergebene Element nicht im Array befindet.`,
 
-  task: `Schreibe eine Funktion <code>add</code>, die ein Element an das Ende eines Arrays hängt.
-Das Element soll aber nur dann angehängt werden, wenn es sich nicht schon im Array befindet.<br><br>Beispiel:
+  task: `Schreibe eine Funktion <code>add</code>, die ein Array und ein Element entgegennimmt und das Element an das Ende des Arrays hängt.
+Das Element soll aber nur dann angehängt werden, wenn es sich nicht schon im Array befindet. Das neue Array soll zurückgegeben werden. <br><br>Beispiel:
 <code>add([1, 2], 3)</code> sollte <code>[1, 2, 3]</code> und <code>add([1, 2], 2)</code> sollte
 <code>[1, 2]</code> zurückgeben.`,
+
+  hint: `<pre><code>function add(arr, elm) {
+  // ...
+  // Benutze if, indexOf() und push() um 
+  // das Element dem Array ggf. hinzuzufügen.
+  // ...
+  return arr;
+}</code></pre>`,
+
+  solution: `<pre><code>function add(arr, elm) {
+  if (arr.indexOf(elm) === -1) {
+    arr.push(elm);
+  }
+  return arr;
+}</code></pre>`,
 
   tests: [
     function() {
