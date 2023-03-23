1841622994cf8258d4548a27611baaee2b4618f9
sdpathak24 <202001258@daiict.ac.in>
2023-03-23 16:16:10 +0530

    Added sol&hints upto q.68 where req

--- www/en/js/koans/array2.js
+++ www/en/js/koans/array2.js
@@ -1,1 +1,31 @@
-jshero.koans.add({id:"array2",title:"Get array elements",lesson:"The elements of an array are accessed by an index beginning with 0.\n\n<pre><code>let languages = ['C', 'C++', 'Java'];\nlet c = languages[0];\nlet cPlusPlus = languages[1];\nlet java = languages[2];</code></pre>",task:"Write a function <code>getFirstElement</code> that takes an array and returns the first element of the array.<br><br>\nExample: <code>getFirstElement([1, 2])</code> should return <code>1</code>.",tests:[function(){return jshero.testutil.assert_isFunction("getFirstElement")},function(){return jshero.testutil.assert_functionHasNumOfParameter("getFirstElement",1)},function(){return jshero.testutil.assert_functionReturns("getFirstElement([1, 2])",1)},function(){return jshero.testutil.assert_functionReturns("getFirstElement(['Firefox', 'Chrome'])","Firefox")}]});
+jshero.koans.add({
+  id: "array2",
+  title: "Get array elements",
+  lesson:
+    "The elements of an array are accessed by an index beginning with 0.\n\n<pre><code>let languages = ['C', 'C++', 'Java'];\nlet c = languages[0];\nlet cPlusPlus = languages[1];\nlet java = languages[2];</code></pre>",
+  task: "Write a function <code>getFirstElement</code> that takes an array and returns the first element of the array.<br><br>\nExample: <code>getFirstElement([1, 2])</code> should return <code>1</code>.",
+  solution: "<pre><code>function getFirstElement(arr){return arr[0];</code></pre>",
+  tests: [
+    function () {
+      return jshero.testutil.assert_isFunction("getFirstElement");
+    },
+    function () {
+      return jshero.testutil.assert_functionHasNumOfParameter(
+        "getFirstElement",
+        1
+      );
+    },
+    function () {
+      return jshero.testutil.assert_functionReturns(
+        "getFirstElement([1, 2])",
+        1
+      );
+    },
+    function () {
+      return jshero.testutil.assert_functionReturns(
+        "getFirstElement(['Firefox', 'Chrome'])",
+        "Firefox"
+      );
+    },
+  ],
+});
--- www/js/koans/array.js
+++ www/js/koans/array.js
@@ -1,1 +1,38 @@
-(function(testutil){jshero.koans.add({id:"array",title:"Arrays",lesson:"Arrays sind Objekte zur Speicherung vieler, meist \xE4hnlicher Werte. Diese sogenannten Elemente werden\nin einem Array der Reihe nach abgelegt. Arrays erzeugt man am besten mit eckigen Klammern.\n\n<pre><code>let sprachen = ['C', 'C++', 'Java', 'JavaScript'];\nlet primzahlen = [2, 3, 5, 7, 11];\nlet leeresArray = [];\nlet a = 'Douglas';\nlet b = 12;\nlet c = true;\nlet allesMoegliche = [a, b, c];</code></pre>\n\nDie Elemente eines Arrays k\xF6nnen, wie das letzte Beispiel zeigt, alle einen anderen Datentyp besitzen.",task:"Schreibe eine Funktion <code>toArray</code>, die 2 Paramter entgegennimmt und diese als Array zur\xFCckgibt.\n<br><br>Beispiel: <code>toArray(5, 9)</code> sollte das Array <code>[5, 9]</code> ergeben.",hint:"<pre><code>function toArray(a, b) {\n  return [...];\n}</code></pre>",solution:"<pre><code>function toArray(a, b) {\n  return [a, b];\n}</code></pre>",tests:[function(){return testutil.assert_isFunction("toArray")},function(){return testutil.assert_functionHasNumOfParameter("toArray",2)},function(){return testutil.assert_functionReturnsType("toArray(1, 2)","Array")},function(){return testutil.assert_functionReturns("toArray(1, 2)",[1,2])},function(){return testutil.assert_functionReturns("toArray('H\xE4nsel', 'Gretel')",["H\xE4nsel","Gretel"])},function(){return testutil.assert_functionReturns("toArray(1, 'Maus')",[1,"Maus"])}]})})(jshero.testutil);
+(function (testutil) {
+  jshero.koans.add({
+    id: "array",
+    title: "Arrays",
+    lesson:
+      "Arrays sind Objekte zur Speicherung vieler, meist \xE4hnlicher Werte. Diese sogenannten Elemente werden\nin einem Array der Reihe nach abgelegt. Arrays erzeugt man am besten mit eckigen Klammern.\n\n<pre><code>let sprachen = ['C', 'C++', 'Java', 'JavaScript'];\nlet primzahlen = [2, 3, 5, 7, 11];\nlet leeresArray = [];\nlet a = 'Douglas';\nlet b = 12;\nlet c = true;\nlet allesMoegliche = [a, b, c];</code></pre>\n\nDie Elemente eines Arrays k\xF6nnen, wie das letzte Beispiel zeigt, alle einen anderen Datentyp besitzen.",
+    task: "Schreibe eine Funktion <code>toArray</code>, die 2 Paramter entgegennimmt und diese als Array zur\xFCckgibt.\n<br><br>Beispiel: <code>toArray(5, 9)</code> sollte das Array <code>[5, 9]</code> ergeben.",
+    hint: "<pre><code>function toArray(a, b) {\n  return [...];\n}</code></pre>",
+    solution:
+      "<pre><code>function toArray(a, b) {\n  return [a, b];\n}</code></pre>",
+    tests: [
+      function () {
+        return testutil.assert_isFunction("toArray");
+      },
+      function () {
+        return testutil.assert_functionHasNumOfParameter("toArray", 2);
+      },
+      function () {
+        return testutil.assert_functionReturnsType("toArray(1, 2)", "Array");
+      },
+      function () {
+        return testutil.assert_functionReturns("toArray(1, 2)", [1, 2]);
+      },
+      function () {
+        return testutil.assert_functionReturns(
+          "toArray('H\xE4nsel', 'Gretel')",
+          ["H\xE4nsel", "Gretel"]
+        );
+      },
+      function () {
+        return testutil.assert_functionReturns("toArray(1, 'Maus')", [
+          1,
+          "Maus",
+        ]);
+      },
+    ],
+  });
+})(jshero.testutil);
