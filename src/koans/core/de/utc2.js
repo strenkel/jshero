82652582af43a7d7412e6c11692823cbd29af85a
Stefan Trenkel <info@stefantrenkel.de>
2023-10-20 22:28:35 +0200

    utc2 completed

--- src/koans/core/de/utc2.js
+++ src/koans/core/de/utc2.js
@@ -13,42 +13,28 @@ Analog zu den sieben lokalen Date-Gettern und -Settern gibt es sieben UTC Date-G
 <code>getUTCMonth()</code> usw. Für obiges Datum liefern die Methoden <code>getUTCFullYear()</code>, <code>getUTCMonth()</code>, <code>getUTCDate()</code>
 und <code>getUTCHours()</code> unabhängig vom Ort die Werte <code>2022</code>, <code>11</code>, <code>31</code> und <code>22</code>.`,
 
-  task: `Schreibe eine Funktion <code>toJSTDateTimeString</code>, die ein Date entgegennimmt und das Datum inkl. Uhrzeit als String in der Japan Standard Time (JST)
-zurückgibt. Die Uhrzeit soll dabei ohne Millisekunden angegeben werden.<br><br>Beispiel: Für <code>date = new Date(Date.UTC(2022, 12, 31, 22, 0))</code> sollte
+  task: `Schreibe eine Funktion <code>toJST</code>, die ein Date entgegennimmt und das Datum inkl. Uhrzeit als String in der
+<a href='https://en.wikipedia.org/wiki/Japan_Standard_Time'>Japan Standard Time (JST)</a>
+zurückgibt. Die Uhrzeit soll dabei ohne Millisekunden angegeben werden.<br><br>Beispiel: Für <code>date = new Date(Date.UTC(2022, 11, 31, 22, 0))</code> sollte
 <code>toJSTDateTimeString(date)</code>den String <code>'2023-01-01T07:00:00 JST'</code> ergeben.`,
 
   tests: [
     function() {
-      return jshero.testutil.assert_isFunction('nikolausUTC');
+      return jshero.testutil.assert_isFunction('toJST');
     },
 
     function() {
-      return jshero.testutil.assert_functionHasNumOfParameter('nikolausUTC', 1);
+      return jshero.testutil.assert_functionHasNumOfParameter('toJST', 1);
+    },
+  
+    function() {
+      return jshero.testutil.assert_functionReturns('toJST(new Date(Date.UTC(2022, 11, 31, 22, 0)))', '2023-01-01T07:00:00 JST');
     },
 
     function() {
-      return jshero.testutil.assert_functionReturnsType('nikolausUTC(2017)', 'Date');
-    },
-
-    function() {
-      return jshero.testutil.assert_functionReturns(
-        'nikolausUTC(2017)',
-        new Date(Date.UTC(2017, 11, 6)), {
-          'utc': true
-        });
-    },
-
-    function() {
-      return jshero.testutil.assert_functionReturnsType('nikolausUTC(1960)', 'Date');
-    },
-
-    function() {
-      return jshero.testutil.assert_functionReturns(
-        'nikolausUTC(1960)',
-        new Date(Date.UTC(1960, 11, 6)), {
-          'utc': true
-        });
+      return jshero.testutil.assert_functionReturns('toJST(new Date(Date.UTC(2023, 0, 1, 0, 1, 1)))', '2023-01-01T09:01:01 JST');
     }
+
   ]
 
 });
