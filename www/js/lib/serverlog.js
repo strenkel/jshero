85089755efc18643fb09a377bdd037a47a7afabb
Stefan Trenkel <info@stefantrenkel.de>
2023-10-22 15:26:24 +0200

    Remove serverlog

--- www/js/lib/serverlog.js
+++
@@ -1,1 +0,0 @@
-if(typeof jshero==="undefined"){var jshero={}}jshero.serverlog=function(){const log=function(id){navigator.sendBeacon("https://www.jshero.net/api/log/".concat(id))};return{log:log}}();
